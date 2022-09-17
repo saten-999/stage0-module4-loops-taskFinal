9753535cedfc5d43eebe07cf016d2541589cb598
Satenik <satabr1999@gmail.com>
2022-09-17 16:36:56 +0400

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        
+        for (int i=0; i< 10; i++){
+        System.out.println((i+1)+ " x " +  numberTableToPrint+ " = " + 
+            (numberTableToPrint * (i+1)));
+        }
     }
 }
